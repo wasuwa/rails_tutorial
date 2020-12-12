b5ede6d711ab2a19a3962d2b40401e4101406c58
baru-web-production <baru.web.production@gmail.com>
2020-12-12 11:01:31 +0000

    Add account activation2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'aqueous-bastion-99357.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
